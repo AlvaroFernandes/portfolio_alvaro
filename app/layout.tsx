4e55d024cbe955db235951c5e50fa30ce07da556
Alvaro Augusto Perdigao Candido Fernandes <fernandes.alvaro@gmail.com>
2024-06-12 09:30:14 +1000

    first commit: add structure and hero page

---
+++ app/layout.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { ThemeProvider } from "./provider";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Alvaro's Portfolio",
+  description:
+    "Hello, I'm Alvaro, a seasoned front-end developer with over 10 years of experience in crafting visually stunning and highly functional web applications."
+};
+
+export default function RootLayout({
+  children
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
